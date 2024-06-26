a1006004df2c5bce51ac487196c277003390b168
shkodransopa <ss41677@ubt-uni.net>
2024-06-27 00:46:53 +0200

    front

---
+++ frontend/src/redux/actions/product.js
@@ -0,0 +1,103 @@
+import axios from "axios";
+import { server } from "../../server";
+
+// create product
+export const createProduct =
+  (
+    name,
+    description,
+    category,
+    tags,
+    originalPrice,
+    discountPrice,
+    stock,
+    shopId,
+    images
+  ) =>
+  async (dispatch) => {
+    try {
+      dispatch({
+        type: "productCreateRequest",
+      });
+
+      const { data } = await axios.post(
+        `${server}/product/create-product`,
+        name,
+        description,
+        category,
+        tags,
+        originalPrice,
+        discountPrice,
+        stock,
+        shopId,
+        images,
+      );
+      dispatch({
+        type: "productCreateSuccess",
+        payload: data.product,
+      });
+    } catch (error) {
+      dispatch({
+        type: "productCreateFail",
+        payload: error.response.data.message,
+      });
+    }
+  };
+
+// get All Products of a shop
+export const getAllProductsShop = (id) => async (dispatch) => {
+  try {
+    dispatch({
+      type: "getAllProductsShopRequest",
+    });
+
+    const { data } = await axios.get(
+      `${server}/product/get-all-products-shop/${id}`
+    );
+    dispatch({
+      type: "getAllProductsShopSuccess",
+      payload: data.products,
+    });
+  } catch (error) {
+    dispatch({
+      type: "getAllProductsShopFailed",
+      payload: error.response.data.message,
+    });
+  }
+};
+
+export const deleteProduct = (id) => async (dispatch) => {
+  dispatch({ type: "deleteProductRequest" });
+  try {
+    const { data } = await axios.delete(`${server}/product/delete-shop-product/${id}`, {
+      withCredentials: true,
+    });
+    dispatch({ type: "deleteProductSuccess", payload: data.message });
+  } catch (error) {
+    console.error("Error during product deletion:", error.response);
+    dispatch({ type: "deleteProductFailed", payload: error.response.data.message });
+    alert("Error during product deletion: " + error.response.data.message);  // Optionally show an alert or handle this in a user-friendly way
+    throw error;  // Re-throw error if needed elsewhere
+  }
+};
+
+
+// get all products
+export const getAllProducts = () => async (dispatch) => {
+  try {
+    dispatch({
+      type: "getAllProductsRequest",
+    });
+
+    const { data } = await axios.get(`${server}/product/get-all-products`);
+    dispatch({
+      type: "getAllProductsSuccess",
+      payload: data.products,
+    });
+  } catch (error) {
+    dispatch({
+      type: "getAllProductsFailed",
+      payload: error.response.data.message,
+    });
+  }
+};
